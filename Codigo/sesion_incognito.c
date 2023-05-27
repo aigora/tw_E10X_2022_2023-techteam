b2f1ca3b9f981a1282e69092cbe58cff121e295f
juliaaragong <julia.aragon@alumnos.upm.es>
2023-05-27 20:16:55 +0200

    cambios media
    he tenido que copiarlos de mi poryecto en c, al parecer no estaba trabajando dentro de github, si no en un documento a parte

--- Codigo/sesion_incognito.c
+++ Codigo/sesion_incognito.c
@@ -30,16 +30,15 @@ int sesionIcognito()
                 return salir;
             }
             break;
-        case '2':
-        	//Hacer calculos
-        	printf("\n Has elegido hacer calculos.\n");
-            printf("Ingresa la fecha (mes-año): ");
-            scanf("%d", &fecha);
-            printf("Ingresa el tipo de generación: ");
-            scanf("%d", &tipoGeneracion);
-            //FALTA CODIGO
+        case '2': 
+        if (calculoDatos(&generacionDatos) == salir)
+            {
+                return salir;
+            }
             break;
-        case '3':
+        case '3': 
+        
+    
             // Añadir nuevos datos
             // FALTA CODIGO
             break;
@@ -128,3 +127,93 @@ int lecturaGeneracion(generacionElectrica *generacionDatos)
     }
     return volver;
 }
+
+
+int calculoDatos(generacionElectrica *generacionDatos)
+{
+    printf("Ha seleccionado hacer calculos\n");
+
+    char tipoGeneracion[Npequeno];
+    int mes1, anio1, mes2, anio2, seleccion_calculo;
+    float media;
+
+    printf("Introduzca el tipo de generacion: ");
+    scanf("%s", tipoGeneracion);
+    
+
+    printf("Introduzca el rango de fechas (mes y año) para los calculos.\n");
+    printf("Fecha 1:\n");
+    printf("Mes: ");
+    scanf("%d", &mes1);
+    printf("Año: ");
+    scanf("%d", &anio1);
+
+    printf("Fecha 2:\n");
+    printf("Mes: ");
+    scanf("%d", &mes2);
+    printf("Año: ");
+    scanf("%d", &anio2);
+   
+	printf("\nHa seleccionado: %s\n", tipoGeneracion);
+    printf("Ha seleccionado las fechas %d/%d - %d/%d\n", mes1, anio1, mes2, anio2);
+ 	printf("------------------------\n");
+    printf("Seleccione que calculo desea hacer: \n");
+    printf("1. Valor medio\n");
+    printf("2. Valor maximo y minimo\n");
+    printf("3. Generacion mas/menos usada\n");
+    printf("4. Porcentaje en la generacion total\n");
+    printf("5. Suma total\n");
+    printf("\"v\" Volver a la ultima pantalla\n");
+    printf("\"q\" Salir del programa\n");
+	
+    scanf("%d", &seleccion_calculo);
+    
+  int i, j; 
+
+    switch (seleccion_calculo)
+    {
+    case 1:
+       printf("Ha seleccionado calcular el valor medio\n");
+    
+    float sumaValores = 0.0;
+    int numero_meses = (anio2 - anio1) * 12 + (mes2 - mes1) + 1;
+
+    for (i = mes1 - 1; i < mes2; i++)
+    {
+        for (j = 0; j < numeroColumnas - 1; j++)
+        {
+            sumaValores += generacionDatos->TiposGeneracion[i].valores[j];
+        }
+    }
+
+    //media
+  
+    float media = sumaValores / numero_meses;
+
+    printf("La media de los datos de %s entre las fechas %d/%d - %d/%d es: %f\n", tipoGeneracion, mes1, anio1, mes2, anio2, media);
+    break;
+    
+    break;
+    
+    
+    case 2:
+            printf("Los valores máximos y mínimos de %s entre las fechas %d/%d - %d/%d son: -------\n", tipoGeneracion, mes1, anio1, mes2, anio2);
+            break;
+    case 3:
+            printf("La generación más usada entre las fechas %d/%d - %d/%d es: -------\n", mes1, anio1, mes2, anio2);
+            break;
+    case 4:
+            printf("El porcentaje en la generación total de %s entre las fechas %d/%d - %d/%d es: -------\n", tipoGeneracion, mes1, anio1, mes2, anio2);
+            break;
+    case 5:
+            printf("La suma total de la generación %s entre las fechas %d/%d - %d/%d es: -------\n", tipoGeneracion, mes1, anio1, mes2, anio2);
+            break;
+    default:
+            printf("El número elegido no es una opción válida, seleccione de nuevo.\n");
+            break;
+    }
+    
+    return volver;
+}
+
+
